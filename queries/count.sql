476690e74076a9e2254012ccbee221d60255f6b4
JeremyLoh <jeremyloh03@gmail.com>
2022-10-07 00:17:25 +0800

    Add query for finding average length of words

--- queries/count.sql
+++ queries/count.sql
@@ -37,3 +37,27 @@ after_word AS (
 SELECT word FROM before_word
 UNION ALL
 SELECT word FROM after_word;
+
+--Find the average length of all words
+SELECT AVG(LENGTH(word))
+FROM words;
+
+-- Find the average length of the first 10000 words, in alphabetical order
+SELECT AVG(LENGTH(word))
+FROM (
+    SELECT word
+    FROM words
+    ORDER BY word ASC
+    LIMIT 10000
+); -- AVG() works on the whole table stated in 'FROM'
+
+-- Find the average length of the words from position 90000 to 108000, in alphabetical order
+-- The average length should be in 2 decimal place
+SELECT ROUND(AVG(LENGTH(word)), 2)
+FROM (
+    SELECT word
+    FROM words
+    ORDER BY word ASC
+    LIMIT 18000
+    OFFSET 90000
+);
