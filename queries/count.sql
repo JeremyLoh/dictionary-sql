632a8faa53b909d2ebcc1ba04769a63d2081283f
JeremyLoh <jeremyloh03@gmail.com>
2022-10-06 23:41:10 +0800

    Add query for finding rows around matching word

--- queries/count.sql
+++ queries/count.sql
@@ -7,7 +7,8 @@ ORDER BY word ASC
 LIMIT 2;
 
 -- Find the previous two words before ball. Maintain the alphabetical order
-SELECT word FROM (
+SELECT word
+FROM (
     SELECT word
     FROM words
     WHERE word < "ball"
@@ -15,3 +16,24 @@ SELECT word FROM (
     LIMIT 2
 )
 ORDER BY word ASC;
+
+-- Find the previous two words before and after applausive. Maintain the alphabetical order
+WITH before_word AS (
+    SELECT word 
+    FROM (SELECT word
+        FROM words
+        WHERE word <= 'applausive'
+        ORDER BY word DESC
+        LIMIT 3)
+    ORDER BY word ASC
+),
+after_word AS (
+    SELECT word
+    FROM words
+    WHERE word > 'applausive'
+    ORDER BY word ASC
+    LIMIT 2
+)
+SELECT word FROM before_word
+UNION ALL
+SELECT word FROM after_word;
